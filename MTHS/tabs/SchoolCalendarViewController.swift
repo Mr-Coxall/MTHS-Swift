b0d75e716fa3a9880c5bfdf591bf70a90b3187e1
Mr.Coxall <mr.coxall@mths.ca>
2016-07-04 23:03:15 -0400

    Added in web view for school calendar.

--- MTHS/tabs/SchoolCalendarViewController.swift
+++ MTHS/tabs/SchoolCalendarViewController.swift
@@ -10,10 +10,17 @@ import UIKit
 
 class SchoolCalendarViewController: UIViewController {
 
+    @IBOutlet weak var schoolCalendarViewController: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        // load school calendar
+        let url = NSURL (string: "https://calendar.google.com/calendar/embed?src=ocsb.ca_sdch4911okio03csdkuaf27beg@group.calendar.google.com&ctz=America/Toronto&pvttk=a3b984a811faad9e768b6735cd65a7bd&mode=AGENDA&showDate=0&showPrint=0&showTabs=0&showCalendars=0&showTz=0&wkst=1&showTitle=0%22")
+        let requestObj = NSURLRequest(URL: url!);
+        schoolCalendarViewController.loadRequest(requestObj);
     }
 
     override func didReceiveMemoryWarning() {
