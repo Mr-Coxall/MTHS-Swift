c3c0e0e5ee6bf71374c69d5a071572c643e317cf
Mr.Coxall <mr.coxall@mths.ca>
2016-07-04 21:55:42 -0400

    Add Google login to the Login page, since during testing it was in the detailed view.

--- MTHS/tabs/LoginViewController.swift
+++ MTHS/tabs/LoginViewController.swift
@@ -8,12 +8,43 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, GIDSignInUIDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        // Google Signin
+        GIDSignIn.sharedInstance().uiDelegate = self
+        
+        var configureError: NSError?
+        GGLContext.sharedInstance().configureWithError(&configureError)
+        
+        if configureError != nil {
+            print(configureError)
+        }
+        
+        
+        //GIDSignIn.sharedInstance().delegate = self
+        
+        let button = GIDSignInButton(frame: CGRectMake(0, 0, 100, 100))
+        button.center = view.center
+        
+        view.addSubview(button)
+        
+    }
+    
+    // Present a view that prompts the user to sign in with Google
+    func signIn(signIn: GIDSignIn!,
+                presentViewController viewController: UIViewController!) {
+        self.presentViewController(viewController, animated: true, completion: nil)
+    }
+    
+    // Dismiss the "Sign in with Google" view
+    func signIn(signIn: GIDSignIn!,
+                dismissViewController viewController: UIViewController!) {
+        self.dismissViewControllerAnimated(true, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
